5f969fca2247526624686a16817b94441e22768a
amanisrajpoot <amanrajpoot59@gmail.com>
2021-10-28 17:47:00 +0530

    11th commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Header from './Header';
 
 export default function Layout() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
