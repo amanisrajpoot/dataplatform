191063cc5639bf78be059ce220b94dfdc7a7b1d5
amanisrajpoot <amanrajpoot59@gmail.com>
2021-12-02 13:18:28 +0530

    25th commit

--- pages/tester.js
+++ pages/tester.js
@@ -1,21 +1,5 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import ScreenLockPortraitOutlinedIcon from '@mui/icons-material/ScreenLockPortraitOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useEffect, useState} from 'react';
-import { useRouter } from 'next/router';
-import { withStyles } from '@material-ui/core/styles';
-import {signIn} from "../function/checkAuth";
 import mixpanel from 'mixpanel-browser';
 import Papa from 'papaparse'
 
